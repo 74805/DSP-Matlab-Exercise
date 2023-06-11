873e54b72fc9127624417a6de0a74aaa885cdec0
74805 <yuvtaub1308@gmail.com>
2023-06-11 19:30:09 +0300

    חלק שני סעיף ב
    Get signal with noise

--- Second_Part.m
+++ Second_Part.m
@@ -1,3 +1,4 @@
+
 % Load the audio file
 [audioSignal, sampleRate] = audioread('audioFile.wav');
 % Resample the audio signal
@@ -7,7 +8,23 @@ N = 2^16;
 audioSignal = audioSignal(1:N);
 
 x = audioSignal;
-
+%soundsc(x, 16000);
 
 % סעיף א
 Px = (1/N) * sum(x.^2);
+
+% סעיף ב
+d1 = 3;
+d2 = 2; % Took the second digit
+
+% Define z[n]
+n = 1:N;
+w1 = 1.6+0.1*d1;
+w2 = 1.6+0.1*d2;
+w3 = 3;
+z = 50*sqrt(Px)*(cos(w1*n)+cos(w2*n)+cos(w3*n));
+
+% Define y[n]
+y = x + z;
+%soundsc(y, 16000);
+% ההקלטה נשמעת חלשה מאוד ועם הרבה רעש ברקע
