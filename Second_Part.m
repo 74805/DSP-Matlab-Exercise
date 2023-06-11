666bdc2210bc374c1fef184608c416d09be75875
74805 <yuvtaub1308@gmail.com>
2023-06-11 19:03:02 +0300

    Create second part
    Load audio file

---
+++ Second_Part.m
@@ -0,0 +1,10 @@
+% Load the audio file
+[audioSignal, sampleRate] = audioread('audioFile.wav');
+% Resample the audio signal
+audioSignal = resample(audioSignal, 16000, sampleRate);
+% Cut signal
+N = 2^16;
+audioSignal = audioSignal(1:N);
+
+x = audioSignal;
+
